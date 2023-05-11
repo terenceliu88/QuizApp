b300d8fd124f4af2241d81b006c12a512fb70d60
terenceliu88 <terenceliu88@gmail.com>
2023-05-11 19:23:06 -0400

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import {defineProps} from "vue"
+
+  const {quiz} = defineProps(['quiz'])
+
+  console.log(quiz)
+</script>
+
+<template>
+    <div class="card">
+        <img :src="quiz.img">
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+  }
+
+  .card .card-text {
+    padding: 0px 0px 10px 10px;
+  }
+
+  .card h2 {
+    font-weight: bold;
+  }
+</style>
